736f367bca81a0aa4dc529ba5a7b73ca770f7422
Wiktor Pabich <202258@edu.p.lodz.pl>
2018-06-17 17:04:30 +0200

    Deleted redundant Console log

--- TypeSpeed/ColorPalette.cs
+++ TypeSpeed/ColorPalette.cs
@@ -12,6 +12,5 @@ namespace TypeSpeed
         public static System.Windows.Media.SolidColorBrush GREEN = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#0A0"));
         public static System.Windows.Media.SolidColorBrush RED = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#C00"));
         public static System.Windows.Media.SolidColorBrush YELLOW = new System.Windows.Media.SolidColorBrush((Color)ColorConverter.ConvertFromString("#CC0"));
-
     }
 }
